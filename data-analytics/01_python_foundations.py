bed4845b9e6994570376b4e881c8be35b92f172d
Gitonga <89454432+blacklightske@users.noreply.github.com>
2026-08-24 15:53:57 +0300

    lesson1

---
+++ data-analytics/01_python_foundations.py
@@ -0,0 +1,7 @@
+sales = [12000, 15000, 9000, 18000, 25000]
+
+print("Number of sales records:", len(sales))
+print("Total sales:", sum(sales))
+print("Average sales:", sum(sales) / len(sales))
+print("Highest sales:", max(sales))
+print("Lowest sales:", min(sales))
