7c0733bd68542b5502f70afce7b85956b3505dff
Gitonga <89454432+blacklightske@users.noreply.github.com>
2026-08-25 13:34:24 +0000

    patching

--- data-analytics/01_python_foundations.py
+++
@@ -1,7 +0,0 @@
-ggitonga170@gmail.comsales = [12000, 15000, 9000, 18000, 25000]
-
-print("Number of sales records:", len(sales))
-print("Total sales:", sum(sales))
-print("Average sales:", sum(sales) / len(sales))
-print("Highest sales:", max(sales))
-print("Lowest sales:", min(sales))
