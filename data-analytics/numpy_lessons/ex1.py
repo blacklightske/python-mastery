0e0010f4fa0c85cb2f967f7624600a2e6c42468e
Gitonga <89454432+blacklightske@users.noreply.github.com>
2026-08-26 13:12:36 +0000

    Add numpy lessons for sales data analysis

---
+++ data-analytics/numpy_lessons/ex1.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+sales = np.array([12000,15000,9000,18000,25000])
+print(sales)
+new=sales*1.1
+print(new)
+ss=sales.sum()
+print(ss)
---
+++ data-analytics/numpy_lessons/numpy_basics.py
@@ -0,0 +1,12 @@
+import numpy as np 
+sales=np.array([12000,15000,9000,18000,25000])
+
+print(sales)
+print("the first value is :",sales[0])
+print("the last value is :",sales[4])
+print("the total is ",sales.sum())
+print("the average is :",sales.mean())
+print("the highet value is:",sales.max())
+print("the lowest value is:",sales.min())
+projected_sales=sales*1.1
+print("increased price is:",projected_sales)
